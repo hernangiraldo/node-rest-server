2803ca55984c4c416343cc9bc6f9ad7c18acb67c
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-11-28 15:29:09 -0400

    Cambio de la uri de MONGO por una variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 // DATA BASE
-const urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/coffee_db' : process.env.MONGO_URI
+const urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/coffee_db' : 'mongodb+srv://BenjiPrice:0o5wqeX76vFGYNyr@cluster0-hveev.mongodb.net/coffee_db'
 process.env.URL_DB = urlDB
